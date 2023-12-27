80147b930aa905c578290927602f8295391e5bc3
Trenton1Fisher <trenton0fisher@gmail.com>
2023-12-26 17:09:40 -0800

    fixed route handler

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,5 @@
-import NextAuth, { AuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import prisma from '@/lib/prismadb'
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  debug: process.env.NODE_ENV === 'development',
-  secret: process.env.NEXTAUTH_SECRET,
-}
+import NextAuth from 'next-auth'
+import { authOptions } from '@/lib/authOptions'
 
 const handler = NextAuth(authOptions)
 
